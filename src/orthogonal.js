7550cad7dbcbf339ab7d67e6b3cb2a40f79aa0fc
Kalle Räisänen <kalle@fivebyfive.se>
2020-10-06 14:11:48 +0200

    Moved some doc to README.md

--- src/orthogonal.js
+++ src/orthogonal.js
@@ -1,47 +1,4 @@
 /**
- * Orthogonal is a JavaScript library providing:
- *  a) dependency injection,
- *  b) a store,
- *  c) a cache, &
- *  d) a bunch of utility methods
- * 
- * The variable `orthogonal` is added to the global scope (I know! I know!),
- * which exposes the functionality.
- * 
- * Some examples:
- * 
-```js
-    // onReady is a utility method, which calls a dependency-injected version
-    // of the function you pass it either directly, if the DOM is loaded, or
-    // on window.load. 
-    orthogonal.onReady(($css, $store) => { // inject $css $store
-        const menuStore = $store.create('menu') // create a new store
-            .state({
-                header: {
-                    isDetached: false,
-                    height: 0
-                },
-                isMobile: false
-            })
-            .actions({
-                updateHeader: (state, {isDetached, height}) => ({...state, header: { isDetached, height }}),
-                updateIsMobile: (state, isMobile) => ({ ...state, isMobile }),
-            });
-
-        // $css contains helpers for working with $css, see `./orthogonal.extensions.js`
-        const [headerClass, detachedClass] = $css.classNames('header', 'detached'); // => ['header', 'header--detached']
-        const header = document.querySelector(`.${headerClass}`);
-        // => $css.getVar() parses and returns css variables
-        const mobileBreakpoint = +$css.getVar('--breakpoint-mobile').replace('px', '');
-
-        // subscribe to changes of the value at the path `header.isDetached` in the store
-        menuStore.select('header.isDetached')
-            .subscribe((detached) => header.classList.toggle(detachedClass, detached));
-    });
-```
- */
-
-/**
  *  @module se/fivebyfive/ortho
  */
 
